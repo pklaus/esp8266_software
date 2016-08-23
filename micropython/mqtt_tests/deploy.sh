01f21663df3dedb6f006077bf395afc30ca83792
Philipp Klaus <philipp.klaus@gmail.com>
2016-08-23 14:12:11 +0200

    update to changed hostname in DHCP

--- micropython/mqtt_tests/deploy.sh
+++ micropython/mqtt_tests/deploy.sh
@@ -4,8 +4,9 @@ source ~/.pyvenv/playground-3.5/bin/activate
 
 PATH=~/Programmierung/webrepl/:$PATH
 #HOST=micropython
-HOST=ESP_D271F5
+#HOST=ESP_D271F5
 #HOST=192.168.4.1
+HOST=nodemcu_01
 
 webrepl_cli.py shark.py     $HOST:/shark.py
 webrepl_cli.py mosquitto.py $HOST:/mosquitto.py
